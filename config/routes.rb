d2ef762754b0fde8511f43a437c380c1c87e3158
Marcelino Llano <info@marze.es>
2009-02-22 00:41:43 +0100

    adjust forms

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ ActionController::Routing::Routes.draw do |map|
   map.logout            '/logout',                    :controller => 'sessions',  :action => 'destroy'
   map.login             '/login',                     :controller => 'sessions',  :action => 'new'
   map.register          '/register',                  :controller => 'users',     :action => 'create'
-  map.signup            '/signup',                    :controller => 'users',     :action => 'new'  
+  map.signup            '/signup',                    :controller => 'users',     :action => 'new'
   map.forgot_password   '/forgot_password',           :controller => 'users',     :action => 'forgot_password'  
   map.reset_password    '/reset_password/:id',        :controller => 'users',     :action => 'reset_password'  
 
