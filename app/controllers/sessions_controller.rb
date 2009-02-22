119f52bceceeb1b37ff1ecd3ad861cb5d7f86144
Marcelino Llano <info@marze.es>
2009-02-22 13:53:50 +0100

    adjust user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,7 +27,7 @@ class SessionsController < ApplicationController
 
   def destroy
     logout_killing_session!
-    flash[:notice] = "You have been logged out."
+    flash[:notice] = "You have been logged out"
     redirect_back_or_default('/')
   end
 
