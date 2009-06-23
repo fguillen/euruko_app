55489ebb1d7183a14c14e881833cd8a5dc14ef2a
Fernando Guillen <fguillen.mail@gmail.com>
2009-06-23 15:23:31 +0200

    closing invoice creation

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -5,6 +5,12 @@ class InvoicesController < ApplicationController
     @cart = Cart.find( params[:id] )                if admin?
     @cart = current_user.carts.find( params[:id] )  if !admin?
     
+    # fguillen 2009-06-23: 
+    # closing the invoice creation
+    flash[:alert] = 'Invoice generation proccess has been closed.'
+    redirect_to :controller => 'carts', :action => 'show', :id => @cart 
+    return
+    
     record_not_found and return  unless @cart.is_purchased?
     record_not_found and return  unless @cart.invoice.nil?
     
